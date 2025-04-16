bee6003fed83e77beb83491bcd316e49b3a8ac69
ronancode1 <ronan.135x@gmail.com>
2025-04-16 10:24:52 -0230

    gb

--- Test 2.py
+++ Test 2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-def print_x_block(times=10):
-    x_row = "X" * 500  # One long row of Xs
+def print_x_block(times=100000):
+    x_row = "X" * 10000  # One long row of Xs
     for _ in range(times):
         st.text(x_row)
 
@@ -9,4 +9,4 @@ st.title("X Printer 3000 🚀")
 
 if st.button("Unleash the Xs"):
     st.write("Brace yourself...")
-    print_x_block(10)  # You can change this number for more rows
+    print_x_block(100000)  # You can change this number for more rows
