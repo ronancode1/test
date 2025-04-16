a7be9dba2196dcbdae5606627cd54c30fca6fb41
ronancode1 <ronan.135x@gmail.com>
2025-04-16 12:54:18 -0230

    k

--- Test 2.py
+++ Test 2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def print_x_block(times):
-    x_row = "X" * 100000000 #Prints one long row of Xs
+    x_row = "M" * 10000000  # Much smaller row
     for _ in range(times):
         st.text(x_row)
 
@@ -9,4 +9,4 @@ st.title("X Printer 3000 🚀")
 
 if st.button("Unleash the Xs"):
     st.write("Brace yourself...")
-    print_x_block(1000000)  # You can change this number for more rows
+    print_x_block(10000)  # Reasonable number of rows
