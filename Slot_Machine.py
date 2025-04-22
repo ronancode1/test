b70bc216533f726d515f95e29c366c00d6512fa2
Ronan White <ronanw135@gmail.com>
2025-04-21 21:30:39 -0230

    sds

--- Slot_Machine.py
+++ Slot_Machine.py
@@ -1,9 +1,53 @@
 # Slot Machine Game
+import random
+
 MAX_LINES = 3
 CARD_BALENCE = 100
 MIN_BET = 1
 MAX_BET = 1000000
 
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "🍀": 1,
+    "💎": 2,
+    "⭐": 3,
+    "🍒": 5,
+    "🪨": 6,
+    "💩": 6,
+    "💣": 4
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+            
+        columns = []
+        for _ in range(cols):
+            column = []
+            current_symbols = all_symbols[:]
+            for _ in range(rows):
+                value = random.choice(current_symbols)
+                current_symbols.remove(value)
+                column.append(value)
+            
+            columns.append(column)
+            
+        return columns
+                
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
+        
+
+    
 def confirm_deposit(a):
     print(f"Your Deposit Amount Is: ${a} \nPlease Confirm")
     while True:
@@ -44,45 +88,46 @@ def deposit():
             print("Please Enter A Number")
             continue
 
-def get_number_of_lines():
-    while True:
-        lines = input("Enter The Number Of Lines To Bet On (1-" + str(MAX_LINES)+ ")?: ")
-        if lines.isdigit():
-            lines = int(lines)
-            if 1 <= lines <= MAX_LINES:
-                break
-            else:
-                print("Enter A Valid Number Of Lines")
-        else:
-            print("Please Enter A Number")
-            continue
-    return lines
-        
-def get_bet():
-    while True:
-        bet = input("What Would You Like To Bet On Each Line? $")
-        if bet.isdigit():
-            bet = int(bet)
-            if MIN_BET <= bet <= MAX_BET:
-                if bet * lines > spending_money:
-                    print(f"You Do Not Have Enough Money To Bet ${bet} On {lines} Lines. Please Enter A Lower Bet.")
-                    continue
-                elif bet * lines <= spending_money:
-                    print(f"Your Bet Of ${bet} On {lines} Lines Has Been Confirmed")
+def Betting():
+    def get_number_of_lines():
+        while True:
+            lines = input("Enter The Number Of Lines To Bet On (1-" + str(MAX_LINES)+ ")?: ")
+            if lines.isdigit():
+                lines = int(lines)
+                if 1 <= lines <= MAX_LINES:
                     break
+                else:
+                    print("Enter A Valid Number Of Lines")
             else:
-                print(f"Please Enter A Bet Between ${MIN_BET} - ${MAX_BET}.")
+                print("Please Enter A Number")
                 continue
-        else:
-            print("Please Enter A Number")
-            continue
-    return bet
+        return lines
+            
+    def get_bet():
+        while True:
+            bet = input("What Would You Like To Bet On Each Line? $")
+            if bet.isdigit():
+                bet = int(bet)
+                if MIN_BET <= bet <= MAX_BET:
+                    if bet * lines > spending_money:
+                        print(f"You Do Not Have Enough Money To Bet ${bet} On {lines} Lines. Please Enter A Lower Bet.")
+                        continue
+                    elif bet * lines <= spending_money:
+                        print(f"Your Bet Of ${bet} On {lines} Lines Has Been Confirmed")
+                        break
+                else:
+                    print(f"Please Enter A Bet Between ${MIN_BET} - ${MAX_BET}.")
+                    continue
+            else:
+                print("Please Enter A Number")
+                continue
+        return bet
 
-spending_money = deposit()  
-lines = get_number_of_lines()
-bet = get_bet()
-total_bet = bet * lines
-print(f"You Are Betting ${bet} On {lines} Lines With A Total Bet Of ${total_bet}")
+    spending_money = deposit()  
+    lines = get_number_of_lines()
+    bet = get_bet()
+    total_bet = bet * lines
+    print(f"You Are Betting ${bet} On {lines} Lines With A Total Bet Of ${total_bet}")
 
          
   
