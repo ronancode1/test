5145b279b6a4cde2e51ddcd5f384bc8ad47fb080
Ronan White <ronanw135@gmail.com>
2025-04-21 21:40:25 -0230

    ds

--- Slot_Machine.py
+++ Slot_Machine.py
@@ -24,19 +24,19 @@ def get_slot_machine_spin(rows, cols, symbols):
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
-            
-        columns = []
-        for _ in range(cols):
-            column = []
-            current_symbols = all_symbols[:]
-            for _ in range(rows):
-                value = random.choice(current_symbols)
-                current_symbols.remove(value)
-                column.append(value)
-            
-            columns.append(column)
-            
-        return columns
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
                 
 def print_slot_machine(columns):
     for row in range(len(columns[0])):
@@ -128,6 +128,8 @@ def Betting():
     bet = get_bet()
     total_bet = bet * lines
     print(f"You Are Betting ${bet} On {lines} Lines With A Total Bet Of ${total_bet}")
+    
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
-         
-  
+Betting()
