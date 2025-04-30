e2eaf898900240ea2a339f05b5a3e393abcdeadf
ronancode1 <ronan.135x@gmail.com>
2025-04-30 13:41:12 -0230

    c

--- Manual_FIle_Sorting.py
+++ Manual_FIle_Sorting.py
@@ -30,7 +30,13 @@ def fix_error(m):
         q1 = input(f"Would You Like To Change The Value Of {m} y/n: ").lower()
         if q1 == "y":
             q2 = input(f"What Would You Like To Change {m} To: ")
-            my_list.append(q2)
+            is_valid = isinstance(q4, str) and all(c.isalpha() or c.isspace() for c in q4)
+            print(f"The Item {q4} is {is_valid}")
+            if is_valid == True:
+                my_list.append(q4)
+            else:
+                fix_error(q4)   
+                my_list.append(q2)
         elif q1 == "n":
             while True:
                 q3 = input("Would You Like To Recreate The List y/n: ")
