ecffb83c814ce07985e007b22eaf1c4d7dda5e5b
ronancode1 <ronan.135x@gmail.com>
2025-04-30 10:30:00 -0230

    jj

--- Manual_FIle_Sorting.py
+++ Manual_FIle_Sorting.py
@@ -1,20 +1,41 @@
-my_list = ["Banana", "Apple", "Orange1"]
+my_list = []
 messedupdigits = []
+
+def createlist():
+    while True:
+        q4 = input("What Would You Like To Add To The List").lower()
+        is_valid = isinstance(q4, str) and all(c.isalpha() or c.isspace() for c in q4)
+        print(f"The Item {q4} is {is_valid}")
+        if is_valid == True:
+            my_list.append(q4)
+        else:
+            fix_error(q4)
+        while True:
+            q5 = input("Would You Like To Add Another Item. y/n: ").lower()
+            if q5 == "y":
+                break
+            elif q5 == "n":
+                sort()
+                break
+            else:
+                continue
+        continue
+
 
 def fix_error(m):
     while True:
         q1 = input(f"Would You Like To Change The Value Of {m} y/n: ").lower()
-        if q1 == y:
+        if q1 == "y":
             q2 = input(f"What Would You Like To Change {m} To: ")
             my_list.remove(m)
             my_list.append(q2)
-        elif q1 == n:
+        elif q1 == "n":
             while True:
                 q3 = input("Would You Like To Recreate The List y/n: ")
-                if q3 == y:
+                if q3 == "y":
                     createlist()
                     break
-                elif q3 == n:
+                elif q3 == "n":
                     break
                 else:
                     print("Enter A Valid Answer")
@@ -24,8 +45,5 @@ def fix_error(m):
             continue
 
 def sort():
-    for i in my_list:
-        is_valid = isinstance(i, str) and all(c.isalpha() or c.isspace() for c in i)
-        print(f"The Item {i} is {is_valid}")
-        fix_error(is_valid)
-sort()
+    print("hi")
+createlist()
