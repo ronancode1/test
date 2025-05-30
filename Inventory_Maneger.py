5fd5b41872c2f8641f3243a6bffa2da98e32f824
ronancode1 <ronan.135x@gmail.com>
2025-05-30 14:34:40 -0230

    jjj

--- Inventory_Maneger.py
+++ Inventory_Maneger.py
@@ -3,10 +3,16 @@ inventory = {
     "cleaning": {},
     "decor": {}
 }
-categorys = ()
-for i in inventory:
-    categorys = categorys + ("," + i)
+categorys = ["food", "cleaning", "decor", "fidget"]
+for i in categorys:
+
 
 def add():
+
     q1 == input("What Do You Want To Add To The Inventory: ")
-    q2 == input(f"What Is The Category Of The Item, The Categorys Are {categorys} ")
+    while True:
+        q2 == input(f"What Is The Category Of The Item, The Categorys Are {categorys} ")
+        for i in 
+    
+    q3 == input(f"What Is The Price Of This Product/Item  'With Decimal Points': ")
+    q3 == "$" + q3
