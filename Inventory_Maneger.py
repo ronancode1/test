f5f61f2bb0f8e79872e4f9be93b646d72e2368d3
ronancode1 <ronan.135x@gmail.com>
2025-05-30 13:56:03 -0230

    hh

--- Inventory_Maneger.py
+++ Inventory_Maneger.py
@@ -6,7 +6,7 @@ inventory = {
 categorys = ()
 for i in inventory:
     categorys = categorys + ("," + i)
-    
+
 def add():
     q1 == input("What Do You Want To Add To The Inventory: ")
-    q2 == input(f"What Is The Category Of The Item ")
+    q2 == input(f"What Is The Category Of The Item, The Categorys Are {categorys} ")
