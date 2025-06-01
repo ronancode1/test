902ead86ce6a8cfe4d643330828e0940e2c0ef4d
Ronan White <ronanw135@gmail.com>
2025-06-01 16:22:51 -0230

    SSADA

--- Inventory_Maneger.py
+++ Inventory_Maneger.py
@@ -4,15 +4,19 @@ inventory = {
     "decor": {}
 }
 categorys = ["food", "cleaning", "decor", "fidget"]
-for i in categorys:
-
 
 def add():
 
-    q1 == input("What Do You Want To Add To The Inventory: ")
+    q1 = input("What Do You Want To Add To The Inventory: ")
     while True:
-        q2 == input(f"What Is The Category Of The Item, The Categorys Are {categorys} ")
-        for i in 
-    
-    q3 == input(f"What Is The Price Of This Product/Item  'With Decimal Points': ")
-    q3 == "$" + q3
+        q2 = input(f"What Is The Category Of The Item, The Categorys Are {categorys} ")
+        if q2 in categorys:
+            break
+        else:
+            print("Invalid category. Please choose from the available categories.")
+            continue
+            
+        q3 = input(f"What Is The Price Of This Product/Item  'With Decimal Points': ")
+        q3 = "$" + q3
+        
+add()
